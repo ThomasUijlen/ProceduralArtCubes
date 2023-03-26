de59b49e00147e11d59e18b72d6584bc508b4881
ThomasUijlen <thomasuijlen@hotmail.com>
2023-03-26 21:18:56 +0200

    Ghost effect

--- Scenes/Main/ProceduralArt.gd
+++ Scenes/Main/ProceduralArt.gd
@@ -2,13 +2,17 @@ extends Spatial
 
 var cubeCount : int = 0
 
+func _ready():
+	$Settings.visible = false
+	$Label.visible = true
+
 func _process(delta):
 	if $"%Cubes".value != cubeCount:
 		setCubes($"%Cubes".value)
 	
 	$Cubes.material_override.set("shader_param/depth", $"%Depth".value)
 	$Cubes.material_override.set("shader_param/depthSampleCount", $"%DepthSampleCount".value)
-	$Cubes.material_override.set("shader_param/depthSampleSize", $"%DepthSampleRadius".value)
+	$Cubes.material_override.set("shader_param/randomOffset", $"%RandomOffset".value)
 
 func setCubes(size : int):
 	self.cubeCount = size
@@ -28,7 +32,7 @@ func setCubes(size : int):
 			var t : Transform = Transform()
 			t.origin = Vector3(x*2, -y*2, 0)-half
 			mesh.set_instance_transform(i, t)
-			mesh.set_instance_custom_data(i, Color(float(x)/float(size),float(y)/float(size),1))
+			mesh.set_instance_custom_data(i, Color(float(x)/float(size),float(y)/float(size),rand_range(0.0,1.0)))
 	
 	$Cubes.multimesh = mesh
 	$Cubes.scale = Vector3.ONE/size*5.0
@@ -43,8 +47,18 @@ func _on_Reset_pressed():
 	$"%Cubes".value = 50
 	$"%Depth".value = 10
 	$"%DepthSampleCount".value = 5
-	$"%DepthSampleRadius".value = 0.01
+	$"%RandomOffset".value = 0
+	$"%Ghost".pressed = false
+	$ViewportColor/ViewportContainer.modulate.a = 1.0
 
 func _input(event):
 	if event.is_action_pressed("Hide"):
 		$Settings.visible = !$Settings.visible
+		$Label.visible = !$Settings.visible
+
+
+func _on_OptionButton_pressed():
+	if $"%Ghost".pressed:
+		$ViewportColor/ViewportContainer.modulate.a = 3.0/255.0
+	else:
+		$ViewportColor/ViewportContainer.modulate.a = 1.0
